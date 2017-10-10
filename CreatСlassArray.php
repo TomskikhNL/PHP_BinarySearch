c2d6386afca2cadf875c0b80bb01dbeccf28c924
Томских Никита Леонидович <tomskih_nl@irkutskoil.ru>
2017-10-10 17:01:03 +0800

    Add Exception
    t

--- CreatСlassArray.php
+++ CreatСlassArray.php
@@ -11,6 +11,9 @@ class Arr
         for ($i=$a; $i<$b; $i++) {
             $this -> array[$i] = rand(0, 100);
         }
+		if (empty($this -> array)){
+			throw new Exception("Массив пуст")!
+		}
         return $this -> array;
     }
 }
