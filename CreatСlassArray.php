ee5a55d9cad3f4c78017d153639d3132c6c14722
Томских Никита Леонидович <tomskih_nl@irkutskoil.ru>
2017-10-11 17:12:19 +0800

    Change Valid

--- CreatСlassArray.php
+++ CreatСlassArray.php
@@ -7,13 +7,13 @@ class Arr
     public $array = [];
     public function __construct($a, $b)
     {
-
+        if ($a<0 OR $b<1 OR $a >$b){
+			throw new \Exception("Некоректные входные данные!");
+		}
         for ($i=$a; $i<$b; $i++) {
             $this -> array[$i] = rand(0, 100);
         }
-		if (empty($this -> array)){
-			throw new Exception("Массив пуст")!
-		}
+		
         return $this -> array;
     }
 }
