544bb12767e6470ae30ed65328131667fe24d9a4
tomskikh.nl <tomskikh.nl@gmail.com>
2017-10-29 22:42:03 +0800

    FixFunctionSearch

--- CreatСlassArray.php
+++ CreatСlassArray.php
@@ -4,47 +4,36 @@ namespace CreateArr;
 
 class Arr
 {
-    public $a;
-    public $b;
     public $array = [];
-    public function __construct($a = 0, $b = 15)
+    public function __construct($a = 0, $b = 100)
     {
-        $this -> a = $a;
-        $this -> b = $b;
         if ($a<0 or $b<1 or $a >$b) {
             throw new \Exception("Некоректные входные данные!");
         }
         for ($i=$a; $i<$b; $i++) {
-            $this -> array[$i] = rand(0, 10);
+            $this -> array[$i] = rand(0, 50);
         }
         return $this -> array;
     }
     public function search($num)
     {
+        $array2 = $this -> array;
+        sort($array2);
         $low = 0;
-        $high = count($this -> array) -1;
-        //var_dump($high);
+        $high = count($array2) -1;
         while ($low <= $high) {
             $mid = floor(($low + $high) / 2);
             var_dump($mid);
-            $guess = $this -> array[$mid];
-            //var_dump($mid);
+            $guess = $array2[$mid];
             if ($num == $guess) {
                 return $mid;
                 break;
             }
             if ($num > $guess) {
-                $low = $mid +1;
+                $low = $mid + 1;
             } else {
-                $high = $mid -1;
+                $high = $mid - 1;
             }
         }
-        //echo "Запрошенное число:" . $num . "не найдено в массиве";
-        //if ($c = 0) {
-        //    echo "Запрошенное число:" . $num . "не найдено в массиве";
-        //}
     }
 }
-$obj = new Arr();
-print_r($obj);
-echo $obj -> search(8) . "===========================";
